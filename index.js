36b821ef8d1aba6ee154f57d2cdfb414b5416cce
Jim Grimes <jimgrimes86@gmail.com>
2023-09-11 13:41:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  let lengthMessage;
+  if(rideLength <= 400) {
+    lengthMessage = "This one is on me!";
+  } else if (rideLength > 400 && rideLength <= 2000) {
+    lengthMessage = "That will be twenty bucks.";
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    lengthMessage = "I will gladly take your thirty bucks.";
+  } else if (rideLength > 2500) {
+    lengthMessage = "No can do.";
+  }
+  return lengthMessage;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much.";
+    case "not as generous" :
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
